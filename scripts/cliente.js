19c8cb5808bef668536e22766f714819ac68dcda
Jessica Fripp <jfripp@farmashop.com.uy>
2024-11-06 13:03:09 -0300

    Formulario de registro y login

--- scripts/cliente.js
+++ scripts/cliente.js
@@ -1,10 +1,10 @@
 class Cliente {
-    constructor(id,nombre,apellido,nombreUsuario,contrasena,numTarjeta,cvc,saldo,millas){
+    constructor(id,nombre,apellido,nombreUsuario,contrasenia,numTarjeta,cvc,saldo,millas){
         this.id = id; 
         this.nombre = nombre; 
         this.apellido = apellido; 
         this.nombreUsuario = nombreUsuario; 
-        this.contrasena = contrasena; 
+        this.contrasenia = contrasenia; 
         this.numTarjeta = numTarjeta; 
         this.cvc = cvc; 
         this.saldo = saldo;
--- scripts/codigo.js
+++ scripts/codigo.js
@@ -1,35 +1,50 @@
 let sistema = new Sistema();
 
+// EVENTOS CLICK
+document.querySelector("#btnLoginUsuario").addEventListener("click", login);
+document.querySelector("#btnRegistroUsuario").addEventListener("click",registrar);
+
 /* REGISTRO */
-let id = "00" + (sistema.clientes.length); 
-let nombreCliente = document.querySelector("#txtNombreCliente").value; 
-let apellidoCliente = document.querySelector("#txtApellidoCliente").value; 
-let nombreUsuarioCliente = document.querySelector("#txtNombreUsuario").value; 
-let contrasenaCliente = document.querySelector("#txtContrasenaRegistro").value; 
-let numTarjeta = document.querySelector("#txtNumeroTarjeta").value;
-let cvc = document.querySelector("#txtNumeroCVC").value; 
-let saldo = 150000;
-let millas = 0; 
 
+function registrar () {
+    let id = "00" + (sistema.clientes.length); 
+    let nombreCliente = document.querySelector("#txtNombreCliente").value; 
+    let apellidoCliente = document.querySelector("#txtApellidoCliente").value; 
+    let nombreUsuarioCliente = document.querySelector("#txtNombreUsuario").value; 
+    let contrasenia = document.querySelector("#txtContrasenaRegistro").value; 
+    let numTarjeta = document.querySelector("#txtNumeroTarjeta").value;
+    let cvc = document.querySelector("#txtNumeroCVC").value; 
+    let saldo = 150000;
+    let millas = 0; 
 
-sistema.registrarCliente(id,nombreCliente,apellidoCliente,nombreUsuarioCliente,contrasenaCliente,numTarjeta,cvc,saldo,millas);
+    
+
+    sistema.registrarCliente(id,nombreCliente,apellidoCliente,nombreUsuarioCliente,contrasenia,numTarjeta,cvc,saldo,millas);
+}
+
 
 // LOGIN 
 
-let nombreUsuario = document.querySelector("#txtUsuario").value;
-let contraseniaUsuario = document.querySelector("#txtContrasenia").value;
-let tipoUsuario = document.querySelector("#slcOpciones").value;
-let pMostrarMensajeLogin = document.querySelector("#pMostrarMensajeLogin").value;
+function login() {
+    let nombreUsuario = document.querySelector("#txtUsuario").value;
+    let contrasenia = document.querySelector("#txtContrasenia").value;
+    let tipoUsuario = document.querySelector("#slcOpciones").value;
+    let pMostrarMensajeLogin = document.querySelector("#pMostrarMensajeLogin");
 
-let resultadoAcceso = sistema.verificarUsuario(nombreUsuario,contraseniaUsuario,tipoUsuario);
+    let usuarioLogin = sistema.login(nombreUsuario, contrasenia, tipoUsuario);
 
-if(resultadoAcceso && tipoUsuario==="C"){
-    // MOSTRAR VISTA CLIENTE 
-}else if (resultadoAcceso && tipoUsuario==="A"){
-    // MOSTRAR VISTA ADMINISTRADOR
-}else {
-    pMostrarMensajeLogin = "Información invalida: Usuario no encontrado"
+    if (usuarioLogin != null && tipoUsuario === "C") {   
+        // MOSTRAR VISTA CLIENTE    
+    } else if (usuarioLogin != null && tipoUsuario === "A") {    
+         // MOSTRAR VISTA ADMIN   
+    } else {
+        pMostrarMensajeLogin.textContent = "Información inválida: Usuario no encontrado";
+    }
 }
+
+
+
+
 
 
 
